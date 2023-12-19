73989e232c7c51c5f575c26a8a7da80ab53e1818
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-12-19 12:40:16 +0100

    glosor och kategorier

--- backend/routes/getwords.ts
+++ backend/routes/getwords.ts
@@ -1,9 +1,30 @@
 // här hämtas glosor
+// lägga in att hämta glosor med ett särskilt category_id
 
 import express from 'express';
 import { client } from '../index';
 
 const router = express.Router();
+
+router.get('/glosor/:category', async (request, response) => {
+  try {
+    const { category } = request.params;
+
+    const query = `
+      SELECT italearnwords.swedish, italearnwords.italian
+      FROM italearnwords
+      JOIN italearncategories ON italearnwords.category_id = italearncategories.categoryId
+      WHERE italearncategories.category = $1;
+    `;
+
+    const { rows } = await client.query(query, [category]);
+    response.json(rows);
+  } catch (error) {
+    console.error('Error fetching data from the database:', error);
+    response.status(500).send('Internal Server Error');
+  }
+});
+
 
 router.get('/glosor', async (_request, response) => {
   try {
@@ -14,5 +35,6 @@ router.get('/glosor', async (_request, response) => {
     response.status(500).send('Internal Server Error');
   }
 });
+
 
 export default router;
